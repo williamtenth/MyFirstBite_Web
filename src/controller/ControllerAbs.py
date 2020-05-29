16f2f60ccbcb7905cc5bb2507656552930da2f3b
williamtenth <williamtenth@gmail.com>
2020-05-28 20:19:00 -0500

    Ajuste en el codigo con los atributos privados de Connector

--- src/controller/ControllerAbs.py
+++ src/controller/ControllerAbs.py
@@ -6,14 +6,6 @@ class ControllerAbs:
     def __init__(self):
         __connector = Connector()
         
-        #__connector.startConnection()
-        # __connector.cursor.execute( "SELECT crm_info_id FROM crm_information Limit 10" )
-        # for crm_info_id in __connector.cursor.fetchall() :
-            # print (crm_info_id)
-            
-
-# def ejecutarConsulta (cadena):
-    # __connector.cursor.execute(cadena)
-    
-
+        __connector.runQuery( "SELECT crm_info_id FROM crm_information Limit 10" )
+        
 con = ControllerAbs()
